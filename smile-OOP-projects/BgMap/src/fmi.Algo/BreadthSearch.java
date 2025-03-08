5a71c5721bb184fc0f5a02bea5bc79a175b407f1
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-03-08 13:30:31 +0200

    Add files via upload

--- smile-OOP-projects/BgMap/src/fmi.Algo/BreadthSearch.java
+++ smile-OOP-projects/BgMap/src/fmi.Algo/BreadthSearch.java
@@ -19,14 +19,14 @@ public class BreadthSearch implements SearchInterfaces {
 	
 	@Override
 	public boolean search(String startName, String goalName) {
-		Node startNode = graph.getNode(goalName);
+		Node startNode = graph.getNode(startName);
 		Node goalNode = graph.getNode(goalName);
-		List<Node> fakeQueue = new ArrayList<>();
+		ArrayList<Node> fakeQueue = new ArrayList<>();
 		
 		
 		if(startNode==null || goalNode==null) 
 		{
-			System.out.println("There are no such nodes in the graph !");
+			System.out.println("Can not start: There are no such nodes in the graph !");
 			return false;
 		}
 		
@@ -45,7 +45,7 @@ public class BreadthSearch implements SearchInterfaces {
 			
 			tempNode.setVisited(true);
 			fakeQueue.remove(0);
-			addNodesToFakeQueue(fakeQueue,tempNode.getLinks());
+			addNodesToFakeQueue(fakeQueue,tempNode);
 			
 		}// end while
 		
@@ -53,12 +53,13 @@ public class BreadthSearch implements SearchInterfaces {
 		return false;
 	}
 	
-	private void addNodesToFakeQueue(List<Node> fakeQueue,List<Link> list) 
+	private void addNodesToFakeQueue(List<Node> fakeQueue,Node parent) 
 	{
-		for(Link l:list) 
+		for(Link l:parent.getLinks()) 
 		{
-			if(l.getRelatedNode().isVisited()==false && fakeQueue.contains(l)) 
+			if(l.getRelatedNode().isVisited()==false && !fakeQueue.contains(l.getRelatedNode())) 
 			{
+				l.getRelatedNode().setParentName(parent.getName()); 
 				fakeQueue.add(l.getRelatedNode());
 			}
 			//fakeQueue.add(l.getRelatedNode());

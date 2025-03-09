429ead49dab198082e17d762b91ae0f5b40d8fa9
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-03-09 12:21:27 +0200

    Add files via upload

--- smile-OOP-projects/BgMap/src/main/Main.java
+++ smile-OOP-projects/BgMap/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import fmi.Algo.BreadthSearch;
 import fmi.Algo.DepthSerch;
+import fmi.Algo.GreedySerchByLinkLenght;
 import fmi.Algo.SearchInterfaces;
 import fmiGraph.Graph;
 import fmiGraph.Node;
@@ -12,9 +13,14 @@ public class Main {
 		
 		Graph g = new Graph();
 		initGraph(g);
-		searchPath("Y","A", new DepthSerch(g));
-		System.out.println("--------------------------------------");
-		searchPath("Y","A", new BreadthSearch(g));
+		System.out.println("-----------Depth Search--------");
+		searchPath("S","A", new DepthSerch(g));
+		System.out.println("-----Breadh search----------");
+		searchPath("S","A", new BreadthSearch(g));
+		System.out.println("-------------Greedy search by link---------");
+		searchPath("S","A",new GreedySerchByLinkLenght(g));
+		
+		
 		
 		
 
@@ -24,11 +30,11 @@ public class Main {
 	{
 		if(method.search(startName, goalName)) 
 		{
-			System.out.println("There is path");
+			System.out.println(String.format("\nThere is path", null));
 		}
 		else {
 			
-			System.out.println("There is no path");
+			System.out.println("n/There is no path");
 		}
 		
 		
@@ -37,33 +43,34 @@ public class Main {
 	
 	public static void initGraph(Graph graph) 
 	{
-		graph.addNode(new Node("S")); // Sofia
-		graph.addNode(new Node("E")); // blagoev grad
-		graph.addNode(new Node("EH")); 
-		graph.addNode(new Node("BT")); 
-		graph.addNode(new Node("P"));
-		graph.addNode(new Node("B")); 
-		graph.addNode(new Node("PB")); 
-		graph.addNode(new Node("ST")); 
-		graph.addNode(new Node("Y")); 
-		graph.addNode(new Node("A")); 
+		graph.addNode(new Node("S",0,8)); // Sofia
+		graph.addNode(new Node("E",1,1)); // blagoev grad
+		graph.addNode(new Node("EH",3,11)); 
+		graph.addNode(new Node("BT",7,10)); 
+		graph.addNode(new Node("P",10,12));
+		graph.addNode(new Node("B",13,9)); 
+		graph.addNode(new Node("PB",4,4)); 
+		graph.addNode(new Node("ST",6,4)); 
+		graph.addNode(new Node("Y",9,2)); 
+		graph.addNode(new Node("A",12,4)); 
+		graph.addNode(new Node("Z",20,14)); 
 		
-//		graph.createBiderectanalLink("S", "E");
-//		graph.createBiderectanalLink("S", "PB");
-//		graph.createBiderectanalLink("S", "EN");
-//		graph.createBiderectanalLink("E", "PB");
-		
-//		graph.createBiderectanalLink("EH", "PB");
-//		graph.createBiderectanalLink("ST", "PB");
-//		graph.createBiderectanalLink("EH", "BT");
-//		graph.createBiderectanalLink("P", "BT");
-//		graph.createBiderectanalLink("ST", "BT");
-//		graph.createBiderectanalLink("B", "BT");
-//		graph.createBiderectanalLink("B", "P");
-//		graph.createBiderectanalLink("B", "A");
-		graph.createBiderectanalLink("Y", "A");
-		graph.createBiderectanalLink("ST", "A");
-		graph.createBiderectanalLink("ST", "Y");
+
+		graph.createBiderectanalLink("S", "E",80);
+		graph.createBiderectanalLink("S", "PB",130);
+		graph.createBiderectanalLink("S", "EH",100);
+		graph.createBiderectanalLink("E", "PB",170);
+		graph.createBiderectanalLink("EH", "PB",150);
+		graph.createBiderectanalLink("ST", "PB",80);
+		graph.createBiderectanalLink("EH", "BT",130);
+		graph.createBiderectanalLink("P", "BT",75);
+		graph.createBiderectanalLink("ST", "BT",90);
+		graph.createBiderectanalLink("B", "BT",200);
+		graph.createBiderectanalLink("B", "P",95);
+		graph.createBiderectanalLink("B", "A",100);
+		graph.createBiderectanalLink("Y", "A",70);
+		graph.createBiderectanalLink("ST", "A",160);
+		graph.createBiderectanalLink("ST", "Y",90);
 	}
 
 }
